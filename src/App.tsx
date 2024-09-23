c7c538c5c6b9840921dfdbb4d86c2509fce850c7
ScarlR <scarletraunak@gmail.com>
2024-09-24 03:28:45 +0530

    set up frontend

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Button>Click me</Button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
